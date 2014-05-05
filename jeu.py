76dc0ca953bdf68f1126e1e637d02323ed2f8dbb
unknown <julienmetivier5@hotmail.com>
2014-05-05 10:57:29 -0400

    ajout des bordures et du message d'erreurs si l'usager touche les bordures

--- jeu.py
+++ jeu.py
@@ -1,32 +1,39 @@
-from tkinter import *
+from    tkinter import  *
+import tkinter.messagebox
+import os
 
-class Carre():
+class   Carre():
 	def __init__(self,parent):
-		self.parent=parent 
-		self.x1=280
-		self.x2=320
-		self.y1=280
-		self.y2=320
+		self.parent=parent  
+		self.x1=(self.parent.largeur/2)-(self.parent.grandeurCarre/2)
+		self.x2=(self.parent.largeur/2)+(self.parent.grandeurCarre/2)
+		self.y1=(self.parent.hauteur/2)-(self.parent.grandeurCarre/2)
+		self.y2=(self.parent.hauteur/2)+(self.parent.grandeurCarre/2)
 		
 	def bouge(self,x,y):
-		self.x1=x-20
-		self.x2=x+20
-		self.y1=y-20
-		self.y2=y+20
+		self.x1=x-(self.parent.grandeurCarre/2)
+		self.x2=x+(self.parent.grandeurCarre/2)
+		self.y1=y-(self.parent.grandeurCarre/2)
+		self.y2=y+(self.parent.grandeurCarre/2)
 
-class Modele():
-	def __init__(self, parent):
-		self.controleur = parent
+class   Modele():
+	def __init__(self,  parent):
+		self.controleur =   parent
+		self.refreshRate = 10
+		self.largeur = 450
+		self.hauteur = 450
+		self.grandeurCarre = 30
+		self.largeurBordure = 50
 		self.carre=Carre(self)
 	
 	def carrebouge(self,x,y):
 		self.carre.bouge(x,y)
 		
-class Controleur():
+class   Controleur():
 	def __init__(self):
 		self.actif=1
-		self.m = Modele(self)
-		self.v = Vue(self)
+		self.m  =   Modele(self)
+		self.v  =   Vue(self)
 		self.v.miseajour(self.m)
 		self.debutPartie()
 		self.v.root.mainloop()
@@ -35,29 +42,30 @@ class Controleur():
 		self.m.carre.bouge(x,y)
 		
 	def debutPartie(self):
-		if self.actif:
+		if  self.actif:
 			self.v.miseajour(self.m)
-			self.v.root.after(50,self.debutPartie)
+			self.v.root.after(self.m.refreshRate,self.debutPartie)
 		
-class Vue():
-	def __init__(self, parent):
-		self.controleur = parent
-		self.root = Tk()
+class   Vue():
+	def __init__(self,  parent):
+		self.controleur =   parent
+		self.root   =   Tk()
 		self.carrebouge=0
-		self.root.wm_title("Carre Rouge")
-		self.canevas=Canvas(self.root,width=450,height=450,bg="grey")
+		self.root.wm_title("Carre   Rouge")
+		self.canevas=Canvas(self.root,width=self.controleur.m.largeur,height=self.controleur.m.hauteur,bg="grey")
 		self.canevas.pack()
 		self.canevas.bind("<Button-1>",self.click)
 		self.canevas.bind("<ButtonRelease>",self.relacherClick)
 		self.canevas.bind("<Motion>",self.bouge)
 		
+		
 	def bouge(self,evt):
-		if self.carrebouge:
+		if  self.carrebouge:
 			self.controleur.carrebouge(evt.x,evt.y)
 		
 	def click(self,evt):
 		lestags=self.canevas.gettags("current")
-		if "carre" in lestags:
+		if  "carre" in  lestags:
 			self.carrebouge=1
 			
 	def relacherClick(self,evt):
@@ -66,7 +74,37 @@ class Vue():
 	def miseajour(self,modele):
 		self.canevas.delete(ALL)
 		j=modele.carre
-		self.canevas.create_rectangle(j.x1,j.y1,j.x2,j.y2,fill="red", tags=("carre"))
+		if  j.x1 <= self.controleur.m.largeurBordure:
+			#j.x1 = self.controleur.m.largeurBordure
+			#j.x2 = self.controleur.m.largeurBordure+self.controleur.m.grandeurCarre
+			tkinter.messagebox.showinfo("Oh non!", "Vous etes mort...")
+			self.root.destroy()
+			os._exit(1)
+		if  j.y1 <= self.controleur.m.largeurBordure:
+			#j.y1 = self.controleur.m.largeurBordure
+			#j.y2 = self.controleur.m.largeurBordure+self.controleur.m.grandeurCarre
+			tkinter.messagebox.showinfo("Oh non!", "Vous etes mort...")
+			self.root.destroy()
+			os._exit(1)
+		if  j.x2 > self.controleur.m.largeur - self.controleur.m.largeurBordure:
+			#j.x1 = (self.controleur.m.largeur-self.controleur.m.largeurBordure) - self.controleur.m.grandeurCarre
+			#j.x2 = self.controleur.m.largeur-self.controleur.m.largeurBordure
+			tkinter.messagebox.showinfo("Oh non!", "Vous etes mort...")
+			self.root.destroy()
+			os._exit(1)
+		if  j.y2 > self.controleur.m.hauteur - self.controleur.m.largeurBordure:
+			#j.y1 = (self.controleur.m.hauteur-self.controleur.m.largeurBordure) - self.controleur.m.grandeurCarre
+			#j.y2 = self.controleur.m.hauteur-self.controleur.m.largeurBordure
+			tkinter.messagebox.showinfo("Oh non!", "Vous etes mort...")
+			self.root.destroy()
+			os._exit(1)
+			
+		self.canevas.create_rectangle(0, 0, self.controleur.m.largeurBordure, self.controleur.m.hauteur, fill="black")
+		self.canevas.create_rectangle(0, self.controleur.m.hauteur-self.controleur.m.largeurBordure, self.controleur.m.largeur, self.controleur.m.hauteur, fill="black")
+		self.canevas.create_rectangle(0, 0, self.controleur.m.largeur, self.controleur.m.largeurBordure, fill="black")
+		self.canevas.create_rectangle(self.controleur.m.largeur-self.controleur.m.largeurBordure, 0, self.controleur.m.largeur, self.controleur.m.hauteur, fill="black")
+		self.canevas.create_rectangle(j.x1,j.y1,j.x2,j.y2,fill="red",   tags=("carre"))
 		
-if __name__ == '__main__':
+		
+if  __name__    ==  '__main__':
 	c=Controleur()
