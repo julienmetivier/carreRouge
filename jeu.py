9dd47ebe304a3707c8e80d93a65d799597591834
unknown <julienmetivier5@hotmail.com>
2014-05-21 12:25:30 -0400

    debut de deplacements de pions + ajout de documentation

--- jeu.py
+++ jeu.py
@@ -26,7 +26,7 @@ class Pion():
 		self.x2=None
 		self.y1=None
 		self.y2=None
-		self.cible = []
+		self.cible=None
 		self.positionInitiale(x,y)
 		
 	def positionInitiale(self,x,y):
@@ -52,48 +52,133 @@ class Pion():
 			self.y2=y+20
 
 	def bouge(self):
-		'''
-		if cible:
-			pass
-		else:
-			x=random.randrange(self.parent.largeur)
-			y=random.randrange(self.parent.hauteur)
-			self.cible=[x,y]
+		blup = None
+		if self.cible != None:
+			if (self.x2-self.x1)/2 == self.cible[0] and (self.y2-self.y1)/2 == self.cible[1]:
+				blup = True
+			else:
+				blup = False
+		if blup == True or self.cible == None:
+			if self.num == 0:
+				if self.parent.nombreCoup%4 == 0:
+					deplacementX = 1
+					deplacementY = 1
+					x=(self.parent.largeur - 15)*deplacementX
+					y=random.randrange(self.parent.hauteur)*deplacementY
+				elif self.parent.nombreCoup%4 == 1:
+					deplacementX = 1
+					deplacementY = -1
+					x=100
+					y=100
+				elif self.parent.nombreCoup%4 == 2:
+					deplacementX = -1
+					deplacementY = -1
+					x=100
+					y=100
+				elif self.parent.nombreCoup%4 == 3:
+					deplacementX = -1
+					deplacementY = 1
+					x=100
+					y=100
+			elif self.num == 1:
+				if self.parent.nombreCoup%4 == 0:
+					deplacementX = -1
+					deplacementY = 1
+					x=100
+					y=100
+				elif self.parent.nombreCoup%4 == 1:
+					deplacementX = -1
+					deplacementY = -1
+					x=100
+					y=100
+				elif self.parent.nombreCoup%4 == 2:
+					deplacementX = 1
+					deplacementY = -1
+					x=100
+					y=100
+				elif self.parent.nombreCoup%4 == 3:
+					deplacementX = 1
+					deplacementY = 1
+					x=100
+					y=100
+			elif self.num == 2:
+				if self.parent.nombreCoup%4 == 0:
+					deplacementX = -1
+					deplacementY = -1
+					x=100
+					y=100
+				elif self.parent.nombreCoup%4 == 1:
+					deplacementX = -1
+					deplacementY = 1
+					x=100
+					y=100
+				elif self.parent.nombreCoup%4 == 2:
+					deplacementX = 1
+					deplacementY = 1
+					x=100
+					y=100
+				elif self.parent.nombreCoup%4 == 3:
+					deplacementX = 1
+					deplacementY = -1
+					x=100
+					y=100
+			elif self.num == 3:
+				if self.parent.nombreCoup%4 == 0:
+					deplacementX = 1
+					deplacementY = -1
+					x=100
+					y=100
+				elif self.parent.nombreCoup%4 == 1:
+					deplacementX = 1
+					deplacementY = 1
+					x=100
+					y=100
+				elif self.parent.nombreCoup%4 == 2:
+					deplacementX = -1
+					deplacementY = 1
+					x=100
+					y=100
+				elif self.parent.nombreCoup%4 == 3:
+					deplacementX = -1
+					deplacementY = -1
+					x=100
+					y=100
 		
 		if self.num == 0:
-			self.x1=x-10
-			self.x2=x+10
-			self.y1=y-10
-			self.y2=y+10
+			self.x1=x-15
+			self.x2=x+15
+			self.y1=y-35
+			self.y2=y+35
 		elif self.num == 1:
-			self.x1=x-10
-			self.x2=x+10
-			self.y1=y-10
-			self.y2=y+10
+			self.x1=x-35
+			self.x2=x+35
+			self.y1=y-12
+			self.y2=y+12
 		elif self.num == 2:
-			self.x1=x-10
-			self.x2=x+10
-			self.y1=y-10
-			self.y2=y+10
+			self.x1=x-15
+			self.x2=x+15
+			self.y1=y-25
+			self.y2=y+25
 		elif self.num == 3:
-			self.x1=x-10
-			self.x2=x+10
-			self.y1=y-10
-			self.y2=y+10
-			'''
+			self.x1=x-20
+			self.x2=x+20
+			self.y1=y-20
+			self.y2=y+20
 		pass
 
 class   Modele():
 	def __init__(self,  parent):
 		self.controleur =   parent
 		self.refreshRate = 10
-		self.largeur = 1200
+		self.largeur = 450
 		self.hauteur = 450
 		self.grandeurCarre = 30
 		self.largeurBordure = 50
 		self.pions = []
 		self.carre=Carre(self)
 		self.creerPions()
+		self.vitesse = 1
+		self.nombreCoup = 0
 	
 	def carreBouge(self,x,y):
 		self.carre.bouge(x,y)
@@ -114,6 +199,27 @@ class   Modele():
 				y = self.hauteur-(self.largeurBordure+40)
 			self.pions.append(Pion(self,x,y,i))
 		
+	def miseajour(self):
+		for i in self.pions:
+			if self.controleur.actif == 1:
+				i.bouge()
+			self.controleur.v.dessinerPion(i)
+		self.nombreCoup+=1	
+			
+	def gestionCollision(self,j):
+		self.controleur.v.canevas.addtag_overlapping("dessus",j.x1, j.y1, j.x2, j.y2)
+		lestags=self.controleur.v.canevas.gettags("dessus")
+		if "pion" in lestags:
+			self.controleur.v.mortSubite()
+		if  j.x1 <= self.largeurBordure:
+			self.controleur.v.mortSubite()
+		if  j.y1 <= self.largeurBordure:
+			self.controleur.v.mortSubite()
+		if  j.x2 > self.largeur - self.largeurBordure:
+			self.controleur.v.mortSubite()
+		if  j.y2 > self.hauteur - self.largeurBordure:
+			self.controleur.v.mortSubite()
+			
 class   Controleur():
 	def __init__(self):
 		self.actif=0
@@ -151,6 +257,11 @@ class   Vue():
 		self.root.destroy()
 		os._exit(1)
 		
+	def mortSubite(self):
+		tkinter.messagebox.showinfo("Oh non!", "Vous etes mort...")
+		#tkMessageBox.showinfo("Oh non!", "Vous etes mort...")
+		self.fermerTout()
+		
 	def initPartie(self):
 		self.canevas=Canvas(self.root,width=self.controleur.m.largeur,height=self.controleur.m.hauteur,bg="grey")
 		self.canevas.pack()
@@ -172,6 +283,9 @@ class   Vue():
 	def relacherClick(self,evt):
 		self.carreBouge=0
 		
+	def dessinerPion(self,pionADessiner):
+		self.canevas.create_rectangle(pionADessiner.x1,pionADessiner.y1,pionADessiner.x2,pionADessiner.y2,fill="blue", tags=("pion"))
+		
 	def miseajour(self,modele):
 		self.canevas.delete(ALL)
 		j=modele.carre
@@ -179,46 +293,9 @@ class   Vue():
 		self.canevas.create_rectangle(0, self.controleur.m.hauteur-self.controleur.m.largeurBordure, self.controleur.m.largeur, self.controleur.m.hauteur, fill="black")
 		self.canevas.create_rectangle(0, 0, self.controleur.m.largeur, self.controleur.m.largeurBordure, fill="black")
 		self.canevas.create_rectangle(self.controleur.m.largeur-self.controleur.m.largeurBordure, 0, self.controleur.m.largeur, self.controleur.m.hauteur, fill="black")
-		for i in modele.pions:
-			if self.controleur.actif == 1:
-				i.bouge()
-			self.canevas.create_rectangle(i.x1,i.y1,i.x2,i.y2,fill="blue", tags=("pion"))
+		self.controleur.m.miseajour()
 		self.canevas.create_rectangle(j.x1,j.y1,j.x2,j.y2,fill="red",   tags=("carre"))
-		self.canevas.addtag_overlapping("dessus",j.x1, j.y1, j.x2, j.y2)
-		lestags=self.canevas.gettags("dessus")
-		if "pion" in lestags:
-			tkinter.messagebox.showinfo("Oh non!", "Vous etes mort...")
-			#tkMessageBox.showinfo("Oh non!", "Vous etes mort...")
-			self.root.destroy()
-			os._exit(1)
-		if  j.x1 <= self.controleur.m.largeurBordure:
-			#j.x1 = self.controleur.m.largeurBordure
-			#j.x2 = self.controleur.m.largeurBordure+self.controleur.m.grandeurCarre
-			tkinter.messagebox.showinfo("Oh non!", "Vous etes mort...")
-			#tkMessageBox.showinfo("Oh non!", "Vous etes mort...")
-			self.root.destroy()
-			os._exit(1)
-		if  j.y1 <= self.controleur.m.largeurBordure:
-			#j.y1 = self.controleur.m.largeurBordure
-			#j.y2 = self.controleur.m.largeurBordure+self.controleur.m.grandeurCarre
-			tkinter.messagebox.showinfo("Oh non!", "Vous etes mort...")
-			#tkMessageBox.showinfo("Oh non!", "Vous etes mort...")
-			self.root.destroy()
-			os._exit(1)
-		if  j.x2 > self.controleur.m.largeur - self.controleur.m.largeurBordure:
-			#j.x1 = (self.controleur.m.largeur-self.controleur.m.largeurBordure) - self.controleur.m.grandeurCarre
-			#j.x2 = self.controleur.m.largeur-self.controleur.m.largeurBordure
-			tkinter.messagebox.showinfo("Oh non!", "Vous etes mort...")
-			#tkMessageBox.showinfo("Oh non!", "Vous etes mort...")
-			self.root.destroy()
-			os._exit(1)
-		if  j.y2 > self.controleur.m.hauteur - self.controleur.m.largeurBordure:
-			#j.y1 = (self.controleur.m.hauteur-self.controleur.m.largeurBordure) - self.controleur.m.grandeurCarre
-			#j.y2 = self.controleur.m.hauteur-self.controleur.m.largeurBordure
-			tkinter.messagebox.showinfo("Oh non!", "Vous etes mort...")
-			#tkMessageBox.showinfo("Oh non!", "Vous etes mort...")
-			self.root.destroy()
-			os._exit(1)
+		self.controleur.m.gestionCollision(j)
 		
 		
 		
